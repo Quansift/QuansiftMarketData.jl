a6e2fdfad2e38436dae356f24abe48979cd97938
10kpw <kojiroh.homma@gmail.com>
2025-02-02 19:12:59 -0600

    export update_split

--- src/TiingoJulia.jl
+++ src/TiingoJulia.jl
@@ -29,7 +29,7 @@ include("fundamental.jl")
 export get_api_key
 export get_ticker_data, download_tickers_duckdb, download_latest_tickers, process_tickers_csv, generate_filtered_tickers
 export connect_duckdb, close_duckdb, update_us_tickers, upsert_stock_data
-export add_historical_data, update_historical, update_splitted_ticker
+export add_historical_data, update_historical, update_split_ticker
 export get_tickers_all, get_tickers_etf, get_tickers_stock
 export connect_postgres, close_postgres, export_to_postgres
 export list_tables
